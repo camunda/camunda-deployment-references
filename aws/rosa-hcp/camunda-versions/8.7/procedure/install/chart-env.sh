7e83466d63de0d7ea5ecc790e055c045ea6366da
Leo J <153937047+leiicamundi@users.noreply.github.com>
2024-12-05 20:32:44 +0100

    add 8.6 for rosa single region

--- aws/rosa-hcp/camunda-versions/8.7/procedure/install/chart-env.sh
+++ aws/rosa-hcp/camunda-versions/8.7/procedure/install/chart-env.sh
@@ -1,3 +1,3 @@
 # The Camunda 8 Helm Chart version
-# renovate: datasource=helm depName=camunda-platform registryUrl=https://helm.camunda.io
+# renovate: datasource=helm depName=camunda-platform versioning=regex:^12(\.(?<minor>\d+))?(\.(?<patch>\d+))?$ registryUrl=https://helm.camunda.io
 export CAMUNDA_HELM_CHART_VERSION="12.0.0"
