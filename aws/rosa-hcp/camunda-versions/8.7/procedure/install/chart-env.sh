3bf932ed50f772422139fd3bc75cfb2a4450c3ba
Leo J <153937047+leiicamundi@users.noreply.github.com>
2024-12-05 21:02:23 +0100

    update values for 8.7

--- aws/rosa-hcp/camunda-versions/8.7/procedure/install/chart-env.sh
+++ aws/rosa-hcp/camunda-versions/8.7/procedure/install/chart-env.sh
@@ -1,3 +1,10 @@
 # The Camunda 8 Helm Chart version
 # renovate: datasource=helm depName=camunda-platform versioning=regex:^12(\.(?<minor>\d+))?(\.(?<patch>\d+))?$ registryUrl=https://helm.camunda.io
-export CAMUNDA_HELM_CHART_VERSION="12.0.0"
+export CAMUNDA_HELM_CHART_VERSION="0.0.0-snapshot-alpha"
+
+# TODO: before the release, update this!
+# TODO: fin a way to abstract the install command
+# helm upgrade --install \                                                      1 (1.606s) < 21:00:15
+          # camunda oci://ghcr.io/camunda/helm/camunda-platform  \
+          # --version 0.0.0-snapshot-alpha --namespace camunda \
+          # -f generated-values.yml
