85741550c0948c042760cade9af34d805eb49c5c
Leo J <153937047+leiicamundi@users.noreply.github.com>
2024-12-06 11:54:38 +0100

    fix host and secrets

--- aws/rosa-hcp/camunda-versions/8.7/procedure/install/create-identity-secret.sh
+++ aws/rosa-hcp/camunda-versions/8.7/procedure/install/create-identity-secret.sh
@@ -2,10 +2,8 @@ kubectl create secret generic identity-secret-for-components \
   --namespace camunda \
   --from-literal=connectors-secret="$CONNECTORS_SECRET" \
   --from-literal=console-secret="$CONSOLE_SECRET" \
-  --from-literal=operate-secret="$OPERATE_SECRET" \
+  --from-literal=core-secret="$CORE_SECRET" \
   --from-literal=optimize-secret="$OPTIMIZE_SECRET" \
-  --from-literal=tasklist-secret="$TASKLIST_SECRET" \
-  --from-literal=zeebe-secret="$ZEEBE_SECRET" \
   --from-literal=admin-password="$ADMIN_PASSWORD" \
   --from-literal=postgres-password="" \
   --from-literal=password="" \
