16652e29cc61cb3e80a03dd82037f68e05feb748
Leo J <153937047+leiicamundi@users.noreply.github.com>
2024-12-13 17:45:05 +0100

    fix new secrets

--- aws/rosa-hcp/camunda-versions/8.7/procedure/install/create-identity-secret.sh
+++ aws/rosa-hcp/camunda-versions/8.7/procedure/install/create-identity-secret.sh
@@ -1,10 +1,13 @@
 kubectl create secret generic identity-secret-for-components \
   --namespace camunda \
-  --from-literal=connectors-secret="$CONNECTORS_SECRET" \
-  --from-literal=console-secret="$CONSOLE_SECRET" \
-  --from-literal=core-secret="$CORE_SECRET" \
-  --from-literal=optimize-secret="$OPTIMIZE_SECRET" \
-  --from-literal=admin-password="$ADMIN_PASSWORD" \
+  --from-literal=identity-connectors-client-token="$CONNECTORS_SECRET" \
+  --from-literal=identity-console-client-token="$CONSOLE_SECRET" \
+  --from-literal=identity-core-client-token="$CORE_SECRET" \
+  --from-literal=identity-optimize-client-token="$OPTIMIZE_SECRET" \
+  --from-literal=identity-keycloak-admin-password="$ADMIN_PASSWORD" \
+  --from-literal=identity-firstuser-password="$FIRST_USER_PASSWORD" \
+  --from-literal=identity-keycloak-postgresql-user-password="$KEYCLOAK_PG_USER_PASSWORD" \
+  --from-literal=identity-keycloak-postgresql-admin-password="$KEYCLOAK_PG_ADMIN_PASSWORD" \
   --from-literal=postgres-password="" \
   --from-literal=password="" \
   --from-literal=smtp-password=""
