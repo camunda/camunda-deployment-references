42298292ba54b4f4aaf353cd8f25bbd5419e0784
Leo J <153937047+leiicamundi@users.noreply.github.com>
2025-01-08 14:39:33 +0100

    export

--- aws/rosa-hcp-dual-region/camunda-version/8.7/procedure/provision/gather-peering-info.sh
+++ aws/rosa-hcp-dual-region/camunda-version/8.7/procedure/provision/gather-peering-info.sh
@@ -31,7 +31,7 @@ CLUSTER_2_PRIVATE_ROUTE_TABLE_IDS_JSON=$(echo "$CLUSTER_2_PRIVATE_ROUTE_TABLE_ID
 
 
 
-OWNER_JSON=$(jq -n \
+export OWNER_JSON=$(jq -n \
   --arg region "$CLUSTER_1_REGION" \
   --arg vpc_cidr_block "$CLUSTER_1_VPC_CIDR" \
   --arg vpc_id "$CLUSTER_1_VPC_ID" \
@@ -47,7 +47,7 @@ OWNER_JSON=$(jq -n \
     private_route_table_ids: $private_route_table_ids
   }')
 
-ACCEPTER_JSON=$(jq -n \
+export ACCEPTER_JSON=$(jq -n \
   --arg region "$CLUSTER_2_REGION" \
   --arg vpc_cidr_block "$CLUSTER_2_VPC_CIDR" \
   --arg vpc_id "$CLUSTER_2_VPC_ID" \
