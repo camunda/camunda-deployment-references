576b5df1999dc7f7a25bd7f590820e098609085a
Leo J <153937047+leiicamundi@users.noreply.github.com>
2025-01-22 21:37:04 +0100

    fix install

--- aws/rosa-hcp-dual-region/camunda-version/8.7/procedure/camunda/install_chart.sh
+++ aws/rosa-hcp-dual-region/camunda-version/8.7/procedure/camunda/install_chart.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
+helm repo add camunda https://helm.camunda.io
+helm repo update
+
 helm upgrade --install \
    "$HELM_RELEASE_NAME" camunda/camunda-platform \
-  --repo https://helm.camunda.io \
   --version "$HELM_CHART_VERSION" \
   --kube-context "$CLUSTER_1_NAME" \
   --namespace "$CAMUNDA_NAMESPACE_1" \
   -f generated-values-region-1.yml
 
-helm upgrade --install
+helm upgrade --install \
   "$HELM_RELEASE_NAME" camunda/camunda-platform \
-  --repo https://helm.camunda.io \
   --version "$HELM_CHART_VERSION" \
   --kube-context "$CLUSTER_2_NAME" \
   --namespace "$CAMUNDA_NAMESPACE_2" \
