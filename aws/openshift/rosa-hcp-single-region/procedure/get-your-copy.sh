332097e31dc02130414fbb1c8f4d49255352530b
Leo J. <153937047+leiicamundi@users.noreply.github.com>
2025-03-07 17:05:46 +0100

    simplify the get

--- aws/openshift/rosa-hcp-single-region/procedure/get-your-copy.sh
+++ aws/openshift/rosa-hcp-single-region/procedure/get-your-copy.sh
@@ -3,20 +3,8 @@
 # Download a copy of the reference architecture
 BRANCH="feature/integrate-tests-rosa"  # TODO: Change the branch to 8.6
 
-# Download the zip file from the specified branch
-curl -L "https://github.com/camunda/camunda-deployment-references/archive/refs/heads/${BRANCH}.zip" -o "camunda-ra.zip"
+git clone --depth 1 --branch "$BRANCH" https://github.com/camunda/camunda-deployment-references.git
 
-
-unzip "camunda-ra.zip" -d temp_extract
-
-# Identify the root folder created by GitHub
-ROOT_FOLDER=$(find temp_extract -mindepth 1 -maxdepth 1 -type d | head -n 1)
-
-# Move only the contents into the target directory
-mkdir -p camunda-deployment-references
-mv temp_extract/"$ROOT_FOLDER"/* camunda-deployment-references/
-rm -Rf temp_extract camunda-ra.zip
-
-# Navigate to the specific directory
+# Navigate to the desired directory
 cd "camunda-deployment-references/aws/openshift/rosa-hcp-single-region" || exit 1
 echo "You are now in the reference architecture directory $(pwd)."
