ff95ff0cdae9a26d66f1f200e57a841a519e81e1
Leo J. <153937047+leiicamundi@users.noreply.github.com>
2025-03-11 14:27:49 +0100

    fix installation step

--- generic/openshift/single-region/procedure/install-chart.sh
+++ generic/openshift/single-region/procedure/install-chart.sh
@@ -7,9 +7,9 @@ helm upgrade --install \
     --version "$CAMUNDA_HELM_CHART_VERSION" --namespace camunda \
     -f generated-values.yml
 
-helm upgrade --install \
-  camunda camunda-platform \
-  --repo https://helm.camunda.io \
-  --version "$CAMUNDA_HELM_CHART_VERSION" \
-  --namespace camunda \
-  -f generated-values.yml
+# helm upgrade --install \
+#   camunda camunda-platform \
+#   --repo https://helm.camunda.io \
+#   --version "$CAMUNDA_HELM_CHART_VERSION" \
+#   --namespace camunda \
+#   -f generated-values.yml
