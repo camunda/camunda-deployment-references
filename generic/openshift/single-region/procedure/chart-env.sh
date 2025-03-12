f7a2c31a3b08331e05fecd2d1c01f1c03d2d28e1
Leo J. <153937047+leiicamundi@users.noreply.github.com>
2025-03-12 17:05:54 +0100

    readify before merge

--- generic/openshift/single-region/procedure/chart-env.sh
+++ generic/openshift/single-region/procedure/chart-env.sh
@@ -2,4 +2,4 @@
 
 # The Camunda 8 Helm Chart version
 # renovate: datasource=helm depName=camunda-platform versioning=regex:^11(\.(?<minor>\d+))?(\.(?<patch>\d+))?$ registryUrl=https://helm.camunda.io
-export CAMUNDA_HELM_CHART_VERSION="11.2.1"
+export CAMUNDA_HELM_CHART_VERSION="11.2.2"
