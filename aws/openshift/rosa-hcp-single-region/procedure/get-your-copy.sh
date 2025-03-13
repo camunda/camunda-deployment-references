7c4e6996bb9760cb0c88d5e50fe2df7d5b3c6a4c
Leo J. <153937047+leiicamundi@users.noreply.github.com>
2025-03-13 17:07:43 +0100

    fix: update version

--- aws/openshift/rosa-hcp-single-region/procedure/get-your-copy.sh
+++ aws/openshift/rosa-hcp-single-region/procedure/get-your-copy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Download a copy of the reference architecture
-BRANCH="feature/rosa-8.7"  # TODO: Change the branch to main then [release-duty] to 8.7
+BRANCH="main"  # TODO: Change the branch to main then [release-duty] to 8.7
 
 git clone --depth 1 --branch "$BRANCH" https://github.com/camunda/camunda-deployment-references.git
 
