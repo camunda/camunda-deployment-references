01f47a6d99295aca108c294321be499616fbc8c6
Langleu <lars.lange@camunda.com>
2025-03-17 16:11:34 +0100

    chore: move some procedures to generic
    chore: remove old maintenance and readme

--- generic/kubernetes/single-region/procedure/chart-env.sh
+++ generic/kubernetes/single-region/procedure/chart-env.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Your standard region that you host AWS resources in
-export REGION="$AWS_REGION"
-
 # The Camunda 8 Helm Chart version
 # renovate: datasource=helm depName=camunda-platform versioning=regex:^12(\.(?<minor>\d+))?(\.(?<patch>\d+))?$ registryUrl=https://helm.camunda.io
 export CAMUNDA_HELM_CHART_VERSION="0.0.0-snapshot-alpha"
