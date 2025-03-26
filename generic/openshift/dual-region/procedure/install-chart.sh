05c4e2772a22822804d47a333b7a3d57b522edc8
Leo <153937047+leiicamundi@users.noreply.github.com>
2025-03-26 22:52:00 +0100

    fix

--- generic/openshift/dual-region/procedure/install-chart.sh
+++ generic/openshift/dual-region/procedure/install-chart.sh
@@ -4,15 +4,34 @@ helm repo add camunda https://helm.camunda.io
 helm repo update
 
 helm upgrade --install \
-   "$HELM_RELEASE_NAME" camunda/camunda-platform \
+  "$HELM_RELEASE_NAME" \
+   oci://ghcr.io/camunda/helm/camunda-platform \
   --version "$HELM_CHART_VERSION" \
   --kube-context "$CLUSTER_1_NAME" \
   --namespace "$CAMUNDA_NAMESPACE_1" \
   -f generated-values-region-1.yml
 
 helm upgrade --install \
-  "$HELM_RELEASE_NAME" camunda/camunda-platform \
+  "$HELM_RELEASE_NAME" \
+   oci://ghcr.io/camunda/helm/camunda-platform \
   --version "$HELM_CHART_VERSION" \
   --kube-context "$CLUSTER_2_NAME" \
   --namespace "$CAMUNDA_NAMESPACE_2" \
   -f generated-values-region-2.yml
+
+# TODO: [release-duty] before the release, update this by removing the oci pull above
+# and uncomment the installation instruction below
+
+# helm upgrade --install \
+#    "$HELM_RELEASE_NAME" camunda/camunda-platform \
+#   --version "$HELM_CHART_VERSION" \
+#   --kube-context "$CLUSTER_1_NAME" \
+#   --namespace "$CAMUNDA_NAMESPACE_1" \
+#   -f generated-values-region-1.yml
+
+# helm upgrade --install \
+#   "$HELM_RELEASE_NAME" camunda/camunda-platform \
+#   --version "$HELM_CHART_VERSION" \
+#   --kube-context "$CLUSTER_2_NAME" \
+#   --namespace "$CAMUNDA_NAMESPACE_2" \
+#   -f generated-values-region-2.yml
