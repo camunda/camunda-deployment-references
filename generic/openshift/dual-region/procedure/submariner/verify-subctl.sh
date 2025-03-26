109e875a8b57eb73f2ef917fef135c2460192835
Leo <153937047+leiicamundi@users.noreply.github.com>
2025-03-26 19:00:37 +0100

    fix: subctl verify

--- generic/openshift/dual-region/procedure/submariner/verify-subctl.sh
+++ generic/openshift/dual-region/procedure/submariner/verify-subctl.sh
@@ -2,14 +2,28 @@
 
 # TODO: integrate in https://docs.camunda.io/docs/8.7/self-managed/setup/deploy/openshift/redhat-openshift-dual-region/#submariner
 
-while true; do
-    STATUS=$(subctl show all --contexts "$CLUSTER_1_NAME,$CLUSTER_2_NAME")
-    echo "$STATUS"
+CLUSTERS=("$CLUSTER_1_NAME" "$CLUSTER_2_NAME")
 
-    # Vérifie que le statut du Gateway et de la Connection est correct
-    if echo "$STATUS" | grep -q "Gateway's status: All connections (1) are established" && \
-       echo "$STATUS" | grep -q "Connection's status: connected"; then
-        echo "Gateway and Connection are correctly established!"
+# Check the status of both clusters
+while true; do
+    last_cluster_ok=false
+
+    for CLUSTER_NAME in "${CLUSTERS[@]}"; do
+        STATUS=$(subctl show all --contexts "$CLUSTER_NAME")
+        echo "Status of Cluster ($CLUSTER_NAME):"
+        echo "$STATUS"
+
+        if echo "$STATUS" | grep -q "All connections (1) are established" && \
+           echo "$STATUS" | grep -q " connected "; then
+            echo "Gateway and Connection for $CLUSTER_NAME are correctly established!"
+            last_cluster_ok=true
+        else
+            echo "$CLUSTER_NAME is not fully connected, retrying..."
+        fi
+    done
+
+    if [ "$last_cluster_ok" = true ]; then
+        echo "Both clusters are correctly connected!"
         exit 0
     fi
 
