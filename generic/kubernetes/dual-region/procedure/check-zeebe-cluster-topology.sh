0d4cac5ed3b835fbb23e940ac446ae057e164528
Leo <153937047+leiicamundi@users.noreply.github.com>
2025-03-26 22:05:45 +0100

    fix

---
+++ generic/kubernetes/dual-region/procedure/check-zeebe-cluster-topology.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# TODO: integrate in the doc
+
+# Starting port-forward in background
+kubectl --context "$CLUSTER_1_NAME" -n "$CAMUNDA_NAMESPACE_1" port-forward "services/$HELM_RELEASE_NAME-zeebe-gateway" 8080:8080 >/dev/null 2>&1 &
+PORT_FORWARD_PID=$!
+
+# Cleanup on exit
+cleanup() {
+  echo "🛑 Stopping port-forward (PID: $PORT_FORWARD_PID)..."
+  kill $PORT_FORWARD_PID
+}
+trap cleanup EXIT
+
+# Wait a bit to ensure port-forward is ready
+sleep 2
+
+# Fetching Zeebe cluster topology
+curl -s -L -X GET 'http://localhost:8080/v2/topology' -H 'Accept: application/json'
