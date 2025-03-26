973c17f849dc7df491896d9ee63a570edf6977a7
Leo <153937047+leiicamundi@users.noreply.github.com>
2025-03-26 15:23:13 +0100

    install subctl instructions

---
+++ generic/openshift/dual-region/procedure/submariner/install-subctl.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Instructions from https://submariner.io/operations/deployment/subctl/
+curl -Ls https://get.submariner.io -o submariner-install.sh
+
+# Review the downloaded script
+cat submariner-install.sh
+
+# renovate: datasource=github-releases depName=submariner-io/subctl
+SUBCTL_VERSION=0.19.0
+
+VERSION="$SUBCTL_VERSION" bash submariner-install.sh
+export PATH=$PATH:~/.local/bin
+echo export PATH=\$PATH:~/.local/bin >> ~/.profile
