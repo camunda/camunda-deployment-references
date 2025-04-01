5b1ddafa7418e9a34d47f3cf6830d7465202023f
Leo <153937047+leiicamundi@users.noreply.github.com>
2025-04-01 12:05:10 +0200

    add some sanity ci rules

--- .lint/trivy/trivy-scan.sh
+++ .lint/trivy/trivy-scan.sh
@@ -8,9 +8,14 @@ set -euo pipefail
 # .trivyignore file in this folder.
 
 # list of the folders that we want to parse, only if a README.md exists and no .trivy_ignore
+current_dir=$(pwd)
 for dir in $(find ./**/modules -type d -maxdepth 1) $(find examples -type d -maxdepth 1); do
+  cd "$current_dir"
   if [ -f "$dir/README.md" ] && ! [ -e "$dir/.trivy_ignore" ]; then
+      cd "$dir"
+      trivy clean --all
+      terraform init || true # try to init if not already initiated
       echo "Scanning terraform module with trivy: $dir"
-      trivy config --config .lint/trivy/trivy.yaml --ignorefile .lint/trivy/.trivyignore "$dir"
+      trivy config --config "${current_dir}/.lint/trivy/trivy.yaml" --ignorefile "${current_dir}/.lint/trivy/.trivyignore" "."
   fi
 done
