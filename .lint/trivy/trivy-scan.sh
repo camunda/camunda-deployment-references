864281ccd3a6e821a2f9d94796908443cf68992b
Leo <153937047+leiicamundi@users.noreply.github.com>
2025-04-01 12:53:14 +0200

    add cache clean

--- .lint/trivy/trivy-scan.sh
+++ .lint/trivy/trivy-scan.sh
@@ -7,6 +7,8 @@ set -euxo pipefail
 # been ignored. For more details, see the
 # .trivyignore file in this folder.
 
+trivy clean --all
+
 # list of the folders that we want to parse, only if a README.md exists and no .trivy_ignore
 for dir in $(find ./**/modules -type d -maxdepth 1) $(find examples -type d -maxdepth 1); do
   if [ -f "$dir/README.md" ] && ! [ -e "$dir/.trivy_ignore" ]; then
