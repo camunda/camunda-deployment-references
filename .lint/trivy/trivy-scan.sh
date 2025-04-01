05aff862bcaf91bd7ba1bc6fe6e027af4b64f3c2
Leo <153937047+leiicamundi@users.noreply.github.com>
2025-04-01 13:26:23 +0200

    clean

--- .lint/trivy/trivy-scan.sh
+++ .lint/trivy/trivy-scan.sh
@@ -7,12 +7,12 @@ set -euxo pipefail
 # been ignored. For more details, see the
 # .trivyignore file in this folder.
 
-rm -Rf "$HOME/.cache/trivy/"
 
 # list of the folders that we want to parse, only if a README.md exists and no .trivy_ignore
 for dir in $(find ./**/modules -type d -maxdepth 1) $(find examples -type d -maxdepth 1); do
   if [ -f "$dir/README.md" ] && ! [ -e "$dir/.trivy_ignore" ]; then
       echo "Scanning terraform module with trivy: $dir"
+      rm -Rf "$HOME/.cache/trivy/"
       trivy config --config .lint/trivy/trivy.yaml --ignorefile .lint/trivy/.trivyignore "$dir"
   fi
 done
