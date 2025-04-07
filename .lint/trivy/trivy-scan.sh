15d8023e036056ae7a0ae821b360b4bb2cdd1142
Langleu <lars.lange@camunda.com>
2025-04-07 13:35:20 +0200

    revert trivy adjustments [ci skip]

--- .lint/trivy/trivy-scan.sh
+++ .lint/trivy/trivy-scan.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -euxo pipefail
+set -euo pipefail
 
 # ⚠️ Warning: This project is not intended for production use but rather for
 # demonstration purposes only. There are no guarantees or warranties provided.
@@ -7,12 +7,10 @@ set -euxo pipefail
 # been ignored. For more details, see the
 # .trivyignore file in this folder.
 
-
 # list of the folders that we want to parse, only if a README.md exists and no .trivy_ignore
 for dir in $(find ./**/modules -type d -maxdepth 1) $(find examples -type d -maxdepth 1); do
   if [ -f "$dir/README.md" ] && ! [ -e "$dir/.trivy_ignore" ]; then
       echo "Scanning terraform module with trivy: $dir"
-      rm -Rf "$HOME/.cache/trivy/"
       trivy config --config .lint/trivy/trivy.yaml --ignorefile .lint/trivy/.trivyignore "$dir"
   fi
 done
