06a6e8ac07b59d292f2d8ef80149092c2995889b
Langleu <lars.lange@camunda.com>
2025-04-07 08:06:57 +0200

    fix: use snapshot release

--- generic/kubernetes/single-region/procedure/install-chart.sh
+++ generic/kubernetes/single-region/procedure/install-chart.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
 helm upgrade --install \
-  camunda camunda-platform \
-  --repo https://helm.camunda.io \
-  --version "$CAMUNDA_HELM_CHART_VERSION" \
-  --namespace camunda \
-  -f generated-values.yml
+    camunda oci://ghcr.io/camunda/helm/camunda-platform \
+    --version "$CAMUNDA_HELM_CHART_VERSION" --namespace camunda \
+    -f generated-values.yml
+
+# TODO: [release-duty] before the release, update this by removing the oci pull above
+# and uncomment the installation instruction below
+
+# helm upgrade --install \
+#   camunda camunda-platform \
+#   --repo https://helm.camunda.io \
+#   --version "$CAMUNDA_HELM_CHART_VERSION" \
+#   --namespace camunda \
+#   -f generated-values.yml
