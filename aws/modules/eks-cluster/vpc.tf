60ff2e1c563a7f8f00514121559f24d0ca3a908d
Langleu <lars.lange@camunda.com>
2025-04-14 14:12:02 +0200

    chore: introduce option single nat gateway for eks

--- aws/modules/eks-cluster/vpc.tf
+++ aws/modules/eks-cluster/vpc.tf
@@ -72,7 +72,7 @@ module "vpc" {
 
   # Single NATGateway per private subnet
   enable_nat_gateway = true
-  single_nat_gateway = false
+  single_nat_gateway = var.single_nat_gateway
   reuse_nat_ips      = false
 
   # Enable DNS hostnames and DNS support required for VPC Peering
