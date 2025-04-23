256c13d52167b2c84439aa30259fd0fa9545557f
Leo <153937047+leiicamundi@users.noreply.github.com>
2025-04-23 21:30:38 +0200

    revert todos

--- generic/openshift/single-region/procedure/chart-env.sh
+++ generic/openshift/single-region/procedure/chart-env.sh
@@ -6,6 +6,5 @@ export CAMUNDA_HELM_CHART_VERSION="0.0.0-snapshot-latest"
 # TODO: [release-duty] before the release, update this!
 # TODO: [release-duty] adjust renovate comment to bump the major version
 
-# TODO: verify all docs reference this and runs smoothly
 export CAMUNDA_NAMESPACE="camunda"
 export CAMUNDA_RELEASE_NAME="camunda"
