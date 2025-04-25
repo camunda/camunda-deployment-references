996cd9bcfdc16b39d06da55bbbe4d433a8b01587
Leo <153937047+leiicamundi@users.noreply.github.com>
2025-04-25 11:27:36 +0200

    tests: integrate rosa domainless tests (#250) (#343)

--- generic/openshift/dual-region/procedure/install-chart.sh
+++ generic/openshift/dual-region/procedure/install-chart.sh
@@ -4,14 +4,14 @@ helm repo add camunda https://helm.camunda.io
 helm repo update
 
 helm upgrade --install \
-  "$HELM_RELEASE_NAME" camunda/camunda-platform \
+  "$CAMUNDA_RELEASE_NAME" camunda/camunda-platform \
   --version "$HELM_CHART_VERSION" \
   --kube-context "$CLUSTER_1_NAME" \
   --namespace "$CAMUNDA_NAMESPACE_1" \
   -f generated-values-region-1.yml
 
 helm upgrade --install \
-  "$HELM_RELEASE_NAME" camunda/camunda-platform \
+  "$CAMUNDA_RELEASE_NAME" camunda/camunda-platform \
   --version "$HELM_CHART_VERSION" \
   --kube-context "$CLUSTER_2_NAME" \
   --namespace "$CAMUNDA_NAMESPACE_2" \
