501413c6c2aa7a5cc516839d84cbdd7e00493d9d
Leo <153937047+leiicamundi@users.noreply.github.com>
2025-04-25 10:20:26 +0200

    remove release duty

--- generic/kubernetes/single-region/procedure/chart-env.sh
+++ generic/kubernetes/single-region/procedure/chart-env.sh
@@ -3,8 +3,6 @@
 # The Camunda 8 Helm Chart version
 # renovate: datasource=helm depName=camunda-platform versioning=regex:^12(\.(?<minor>\d+))?(\.(?<patch>\d+))?$ registryUrl=https://helm.camunda.io
 export CAMUNDA_HELM_CHART_VERSION="12.0.0"
-# TODO: [release-duty] before the release, update this!
-# TODO: [release-duty] adjust renovate comment to bump the major version
 
 export CAMUNDA_NAMESPACE="camunda"
 export CAMUNDA_RELEASE_NAME="camunda"
