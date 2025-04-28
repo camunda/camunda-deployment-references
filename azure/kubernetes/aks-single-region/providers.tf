491d616fa73d454cab1d01ff69833bf81ade62f6
Balazs Kenez <96087935+bkenez@users.noreply.github.com>
2025-04-28 14:08:28 +0200

    fix: try registering feature

--- azure/kubernetes/aks-single-region/providers.tf
+++ azure/kubernetes/aks-single-region/providers.tf
@@ -13,3 +13,22 @@ provider "azurerm" {
   features {}
   subscription_id = var.subscription_id
 }
+
+resource "azurerm_resource_provider_registration" "container_service" {
+  provider_namespace = "Microsoft.ContainerService"
+}
+
+
+resource "azurerm_resource_provider_registration" "aks_apiserver_vnet_preview" {
+  provider_namespace = "Microsoft.ContainerService"
+
+  feature {
+    name       = "EnableAPIServerVnetIntegrationPreview"
+    registered = true
+  }
+
+  # ensure the provider is registered first
+  depends_on = [
+    azurerm_resource_provider_registration.container_service
+  ]
+}
