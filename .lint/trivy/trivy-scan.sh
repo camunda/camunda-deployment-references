e2efbc80e084b8fff1b0ea4d0eed4bce21f3b6a6
Leo <153937047+leiicamundi@users.noreply.github.com>
2025-05-13 11:04:00 +0200

    fix trivy scan

--- .lint/trivy/trivy-scan.sh
+++ .lint/trivy/trivy-scan.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-set -euxo pipefail
+set -euo pipefail
 
-# list of the folders that we want to parse, only if a README.md exists and no .trivy_ignore
-echo "Scanning terraform files in aws dir with trivy:"
-trivy config --config .lint/trivy/trivy.yaml --ignorefile .trivyignore --skip-dirs aws/ec2/test/fixtures aws
+# list of the folders that we want to parse, only if a README.md exists and no .trivy_ignore and at least one tf file is in the directory
+# the .test, test and fixtures directories are excluded
+find . \( -type d \( -name .terraform -o -name .test -o -name test -o -name fixtures \) \) -prune -false -o -type d -print | while read -r dir; do
+  if [ -f "$dir/README.md" ] && [ ! -e "$dir/.trivy_ignore" ] && ls "$dir"/*.tf >/dev/null 2>&1; then
+      echo "Scanning terraform module with trivy: $dir"
+
+      trivy config --config .lint/trivy/trivy.yaml --ignorefile .lint/trivy/.trivyignore "$dir"
+  fi
+done
