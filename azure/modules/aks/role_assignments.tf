09345eaac260d5e6aaab94293ad12e6978e82518
Balazs Kenez <96087935+bkenez@users.noreply.github.com>
2025-05-22 16:56:47 +0200

    fix: specify principal_type

--- azure/modules/aks/role_assignments.tf
+++ azure/modules/aks/role_assignments.tf
@@ -2,4 +2,5 @@ resource "azurerm_role_assignment" "dns_contributor" {
   scope                = var.dns_zone_id
   role_definition_name = "DNS Zone Contributor"
   principal_id         = var.uami_object_id
+  principal_type       = "ServicePrincipal"
 }
