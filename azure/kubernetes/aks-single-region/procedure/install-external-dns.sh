7b0e272f570448a8e0f655746e2aa1afbdaf0d38
Balazs Kenez <96087935+bkenez@users.noreply.github.com>
2025-05-28 15:26:25 +0200

    fix: externaldns args

--- azure/kubernetes/aks-single-region/procedure/install-external-dns.sh
+++ azure/kubernetes/aks-single-region/procedure/install-external-dns.sh
@@ -7,8 +7,8 @@ helm upgrade --install \
   --set provider=azure \
   --set txtOwnerId="${EXTERNAL_DNS_OWNER_ID:-external-dns}" \
   --set policy=sync \
-  --set azure.resourceGroup="$AZURE_DNS_RESOURCE_GROUP" \
-  --set azure.subscriptionId="$AZURE_SUBSCRIPTION_ID" \
-  --set azure.useManagedIdentity=true \
+  --set "extraArgs[0]=--azure-resource-group=rg-infraex-global-permanent" \
+  --set "extraArgs[1]=--azure-subscription-id=5667840f-dd25-4fe1-99ee-5e752ec80b5c" \
+  --set "extraArgs[2]=--azure-use-managed-identity=true" \
   --namespace external-dns \
   --create-namespace
