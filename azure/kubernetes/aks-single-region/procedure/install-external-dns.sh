f5fdfb4a4e9771bafcbbb4e9cc041e78d5fb7e3c
Balazs Kenez <96087935+bkenez@users.noreply.github.com>
2025-05-28 16:19:37 +0200

    fix: externaldns attempt

--- azure/kubernetes/aks-single-region/procedure/install-external-dns.sh
+++ azure/kubernetes/aks-single-region/procedure/install-external-dns.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-helm upgrade --install \
-  external-dns external-dns \
+helm upgrade --install external-dns external-dns \
   --repo https://kubernetes-sigs.github.io/external-dns/ \
   --version "$EXTERNAL_DNS_HELM_CHART_VERSION" \
+  --namespace external-dns --create-namespace \
   --set provider=azure \
-  --set txtOwnerId="${EXTERNAL_DNS_OWNER_ID:-external-dns}" \
   --set policy=sync \
+  --set txtOwnerId=external-dns-camunda-470-rfo3h-aks-aks-single-region \
+  --set azure.resourceGroup=rg-infraex-global-permanent \
+  --set azure.subscriptionId=5667840f-dd25-4fe1-99ee-5e752ec80b5c \
+  --set azure.useManagedIdentity=true \
   --set "extraArgs[0]=--azure-resource-group=rg-infraex-global-permanent" \
   --set "extraArgs[1]=--azure-subscription-id=5667840f-dd25-4fe1-99ee-5e752ec80b5c" \
-  --set "extraArgs[2]=--azure-use-managed-identity=true" \
-  --namespace external-dns \
-  --create-namespace
+  --set "extraArgs[2]=--azure-config-file=/etc/kubernetes/azure.json" \
+  --set extraVolumes[0].name=azure-config-file \
+  --set extraVolumes[0].secret.secretName=azure-config-file \
+  --set extraVolumeMounts[0].name=azure-config-file \
+  --set extraVolumeMounts[0].mountPath=/etc/kubernetes \
+  --set extraVolumeMounts[0].readOnly=true
