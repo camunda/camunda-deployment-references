b5d7dc9aa1f05333fc2f9bf5c4c4b5b9fa299b32
Balazs Kenez <96087935+bkenez@users.noreply.github.com>
2025-06-16 15:29:58 +0200

    fix: simplified scripts/test following simplified docs

--- azure/kubernetes/aks-single-region/procedure/tfvars-domain.sh
+++ azure/kubernetes/aks-single-region/procedure/tfvars-domain.sh
@@ -2,8 +2,6 @@
 
 export AZURE_SUBSCRIPTION_ID=$(az account show --query "id" -o tsv)
 
-export AZURE_SP_ID=$(az ad sp list --display-name "$AZURE_SP_NAME" --query "[0].appId" -o tsv)
-
 export AZURE_DNS_ZONE_ID=$(az network dns zone show \
   --name "$TLD" \
   --resource-group "$AZURE_DNS_RESOURCE_GROUP" \
--- azure/kubernetes/aks-single-region/procedure/tfvars-no-domain.sh
+++ azure/kubernetes/aks-single-region/procedure/tfvars-no-domain.sh
@@ -2,8 +2,6 @@
 
 export AZURE_SUBSCRIPTION_ID=$(az account show --query "id" -o tsv)
 
-export AZURE_SP_ID=$(az ad sp list --display-name "$AZURE_SP_NAME" --query "[0].appId" -o tsv)
-
 cat <<EOF > terraform.tfvars
 subscription_id     = "$AZURE_SUBSCRIPTION_ID"
 terraform_sp_app_id = "$AZURE_SP_ID"
