4b132b09e9a1bf16b3a7099e21e05bd547a42b4e
Leo <153937047+leiicamundi@users.noreply.github.com>
2025-06-17 11:40:24 +0200

    fix

--- .github/scripts/custom-bash.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# custom-bash.sh
-# Charge les fonctions, puis exécute le script temporaire passé via {0}
-
-# shellcheck disable=SC1091
-source "./gha-functions.sh"
- /usr/bin/bash -e "$@"
