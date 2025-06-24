96a1bde3a9692838b0f0c044d9ba0f7abd629831
Langleu <lars.lange@camunda.com>
2025-06-24 13:50:55 +0200

    feat: add generic tf outputs action

--- aws/compute/ec2-single-region/terraform/test/fixtures/ci/fixture_local_override.tf
+++ aws/compute/ec2-single-region/terraform/test/fixtures/ci/fixture_local_override.tf
@@ -2,4 +2,6 @@
 locals {
   generate_ssh_key_pair = "true"
   opensearch_enable     = "false" # TODO: Temporary for the tests, remove when ready
+  enable_alb            = "false" # TODO: Temporary for the tests, remove when ready
+  enable_nlb            = "false" # TODO: Temporary for the tests, remove when ready
 }
