2d8ea18c4b3ef818a47acccce6751368d67a0ae3
Langleu <lars.lange@camunda.com>
2025-06-27 09:07:06 +0200

    chore: use 8.6 and enable everything again

--- aws/compute/ec2-single-region/terraform/test/fixtures/ci/fixture_local_override.tf
+++ aws/compute/ec2-single-region/terraform/test/fixtures/ci/fixture_local_override.tf
@@ -1,7 +1,4 @@
 # Overwrite the default values for the locals used in ci tests.
 locals {
   generate_ssh_key_pair = "true"
-  opensearch_enable     = "false" # TODO: Temporary for the tests, remove when ready
-  enable_alb            = "false" # TODO: Temporary for the tests, remove when ready
-  enable_nlb            = "false" # TODO: Temporary for the tests, remove when ready
 }
