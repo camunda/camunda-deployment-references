d732fef38598abc00d6d2938b75f9279ae6346d9
Leo <153937047+leiicamundi@users.noreply.github.com>
2025-07-02 13:28:24 +0200

    fix ttl

--- aws/modules/opensearch/domain.tf
+++ aws/modules/opensearch/domain.tf
@@ -45,7 +45,7 @@ resource "aws_acmpca_certificate" "signed_cert" {
 
   validity {
     type  = "DAYS"
-    value = 365
+    value = 180
   }
 
   template_arn = "arn:aws:acm-pca:::template/EndEntityCertificate/V1"
