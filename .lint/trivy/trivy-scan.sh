db269af2c78e4611b23def000964358d3e377a8f
Langleu <lars.lange@camunda.com>
2025-07-02 13:42:57 +0200

    fix: trivy to properly ignore fixtures and test folders

--- .lint/trivy/trivy-scan.sh
+++ .lint/trivy/trivy-scan.sh
@@ -3,10 +3,10 @@ set -euo pipefail
 
 # list of the folders that we want to parse, only if a README.md exists and no .trivy_ignore and at least one tf file is in the directory
 # the .test, test and fixtures directories are excluded
-find . \( -type d \( -name .terraform -o -name .test -o -name test -o -name fixtures \) \) -prune -false -o -type d -print | while read -r dir; do
+find . -type d \( -name .terraform -o -name .test -o -name test -o -name fixtures \) -prune -o -type d -print | while read -r dir; do
   if [ -f "$dir/README.md" ] && [ ! -e "$dir/.trivy_ignore" ] && ls "$dir"/*.tf >/dev/null 2>&1; then
-      echo "Scanning terraform module with trivy: $dir"
-
-      trivy config --config .lint/trivy/trivy.yaml --ignorefile .lint/trivy/.trivyignore "$dir"
+    echo "Scanning terraform module with trivy: $dir"
+    # explicitly skip the .terraform, .test, test and fixtures directories, they may still be parts of found directories and scanned recursively
+    trivy config --skip-dirs ".terraform,.test,test,fixtures" --config .lint/trivy/trivy.yaml --ignorefile .lint/trivy/.trivyignore "$dir"
   fi
 done
