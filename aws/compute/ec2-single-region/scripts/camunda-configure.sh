4e924f943f9c50f0b0ba58c3781d8bce3325913c
Langleu <lars.lange@camunda.com>
2025-07-03 16:09:03 +0200

    chore: persist 8.8 changes so far

--- aws/compute/ec2-single-region/scripts/camunda-configure.sh
+++ aws/compute/ec2-single-region/scripts/camunda-configure.sh
@@ -22,7 +22,6 @@ echo "[INFO] Configuring the environment variables for cluster communication, ex
     echo "CAMUNDA_OPERATE_ZEEBEOPENSEARCH_URL=\"${OPENSEARCH_URL}\""
     echo "CAMUNDA_TASKLIST_OPENSEARCH_URL=\"${OPENSEARCH_URL}\""
     echo "CAMUNDA_TASKLIST_ZEEBEOPENSEARCH_URL=\"${OPENSEARCH_URL}\""
-    echo "ZEEBE_BROKER_EXPORTERS_OPENSEARCH_ARGS_URL=\"${OPENSEARCH_URL}\""
     echo "ZEEBE_BROKER_EXPORTERS_CAMUNDAEXPORTER_ARGS_CONNECT_URL=\"${OPENSEARCH_URL}\""
     echo "CAMUNDA_DATABASE_URL=\"${OPENSEARCH_URL}\""
     # Disable old importers
@@ -31,19 +30,6 @@ echo "[INFO] Configuring the environment variables for cluster communication, ex
     echo "CAMUNDA_TASKLIST_IMPORTERENABLED=\"false\""
     echo "CAMUNDA_TASKLIST_ARCHIVERENABLED=\"false\""
 } >> "${CURRENT_DIR}/camunda-environment.tmp"
-
-# Disabling problematic importers and archivers in Operate and Tasklist
-# These are not HA compatbile and can only run once but we keep the WebUI
-
-# if index is even and total count is more than 1 or index is greater than 2
-# disable exporters for those instances
-# essentially we just want to keep the exporter on the first uneven instance
-# While keeping it flexible to use different amount of nodes.
-# if (( index % 2 == 0 && total_ip_count > 1 )) || (( index > 2 )); then
-#   {
-    # will be required to disable importer / archiver when Optimize is included
-#   } >> "${CURRENT_DIR}/camunda-environment.tmp"
-# fi
 
 if [[ $SECURITY == 'false' ]]; then
   echo "[INFO] Configuring Connectors to use plain text communication."
