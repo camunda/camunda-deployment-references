36215efba9353ffb8e749614857542b8445fa84b
Leo <153937047+leiicamundi@users.noreply.github.com>
2025-07-10 12:11:57 +0200

    fix

--- generic/kubernetes/single-region/procedure/install-chart.sh
+++ generic/kubernetes/single-region/procedure/install-chart.sh
@@ -48,7 +48,7 @@ spec:
 EOF
 
 helm upgrade --install \
-    "$CAMUNDA_RELEASE_NAME" oci://ghcr.io/camunda/helm/camunda-platform \
+    "$CAMUNDA_RELEASE_NAME" camunda-platform --repo https://helm.camunda.io  \
     --version "$CAMUNDA_HELM_CHART_VERSION" --namespace "$CAMUNDA_NAMESPACE" \
     -f generated-values.yml
 
