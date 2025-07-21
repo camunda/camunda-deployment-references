b4f2af2728260959146897943b1897a3408b029e
Langleu <lars.lange@camunda.com>
2025-07-21 08:21:49 +0200

    fix: revert ingress-nginx to lower version
    ingress-nginx 4.13.0 contains some changes that beak the environment, patches are still pending for the fix

--- generic/kubernetes/single-region/procedure/export-ingress-setup-vars.sh
+++ generic/kubernetes/single-region/procedure/export-ingress-setup-vars.sh
@@ -7,7 +7,7 @@ export MAIL=admin@camunda.example.com
 # Helm chart versions for Ingress components
 
 # renovate: datasource=helm depName=ingress-nginx registryUrl=https://kubernetes.github.io/ingress-nginx
-export INGRESS_HELM_CHART_VERSION="4.13.0"
+export INGRESS_HELM_CHART_VERSION="4.12.3"
 # renovate: datasource=helm depName=external-dns registryUrl=https://kubernetes-sigs.github.io/external-dns/
 export EXTERNAL_DNS_HELM_CHART_VERSION="1.18.0"
 # renovate: datasource=helm depName=cert-manager registryUrl=https://charts.jetstack.io
