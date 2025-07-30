cfdae32c7575e5325c3dbd82782c240b2931cad9
Langleu <lars.lange@camunda.com>
2025-07-30 12:04:16 +0200

    fix: mv tmp files to differenet location to be able to resolve access

--- generic/compute/debian/procedure/camunda-services.sh
+++ generic/compute/debian/procedure/camunda-services.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+CONFIG_DIR="${SCRIPT_DIR}/../configs"
 MNT_DIR="${MNT_DIR:-"/opt/camunda"}"
 
 source "${SCRIPT_DIR}/helpers.sh"
@@ -9,15 +10,15 @@ source "${SCRIPT_DIR}/helpers.sh"
 # Copies the final configuration files to the remote instance and starts the Camunda 8 services.
 
 echo "[INFO] Copying the configuration files to the remote server."
-transfer_file "${SCRIPT_DIR}/../camunda-environment.tmp" "${MNT_DIR}/camunda/config/camunda-environment" camunda-environment.tmp
-rm -rf "${SCRIPT_DIR}/../camunda-environment.tmp"
+transfer_file "${SCRIPT_DIR}/camunda-environment.tmp" "${MNT_DIR}/camunda/config/camunda-environment" camunda-environment.tmp
+rm -rf "${SCRIPT_DIR}/camunda-environment.tmp"
 
 transfer_file "${CURRENT_DIR}/connectors-environment.tmp" "${MNT_DIR}/connectors/connectors-environment" connectors-environment.tmp
-rm -rf "${SCRIPT_DIR}/../connectors-environment.tmp"
+rm -rf "${SCRIPT_DIR}/connectors-environment.tmp"
 
 echo "[INFO] Installing the Camunda 8 systemd service on the remote server."
-transfer_file "${SCRIPT_DIR}/../configs/camunda.service" "${MNT_DIR}" camunda.service
-transfer_file "${SCRIPT_DIR}/../configs/connectors.service" "${MNT_DIR}" connectors.service
+transfer_file "${CONFIG_DIR}/camunda.service" "${MNT_DIR}" camunda.service
+transfer_file "${CONFIG_DIR}/connectors.service" "${MNT_DIR}" connectors.service
 
 remote_cmd "sudo mv ${MNT_DIR}/camunda.service /etc/systemd/system/camunda.service"
 remote_cmd "sudo mv ${MNT_DIR}/connectors.service /etc/systemd/system/connectors.service"
