1c7c8f4fbd00a4abdf78a75265c5d494392a3346
Lars Lange <9141483+Langleu@users.noreply.github.com>
2025-08-12 13:03:51 +0200

    chore: move ec2 related items to generic/debian where applicable (#840)
    * chore: move configs and create symlink
    
    add on top of infraex-770 and 772
    
    * chore: minor ec2 script adjustments
    commit on top of infraex-770 and 772
    
    * chore: remove script folder and move to procedure
    
    commit on top of infraex-770 and 772
    
    * chore: rearrange some files
    
    commit on top of infraex-770 and 772
    
    * chore: ignore resulting tmp files of ec2
    
    * fix: mv tmp files to differenet location to be able to resolve access

--- generic/compute/debian/procedure/camunda-install.sh
+++ generic/compute/debian/procedure/camunda-install.sh
@@ -6,6 +6,7 @@ set -euo pipefail
 
 # Example usage via jump host
 # ssh -J admin@BASTION_IP admin@CAMUNDA_IP < ./camunda-install.sh
+# Or wget the script on the remote host and execute it directly
 
 # Executed on remote host, defaults should be set here or env vars preconfigured on remote host
 OPENJDK_VERSION=${OPENJDK_VERSION:-"21"}
@@ -58,8 +59,7 @@ if id -nG "$USERNAME" | grep -qw "$USERNAME"; then
 fi
 
 # Configs will automatically be newly generated etc. based on this repo / customer changes.
-# The following may be enough already, will need proper Camunda alpha versions to test it.
-# Backup of data folder may be useless since the new version will migrate the database and render any older version usless.
+# Upgrade will explicitly remove the old libs and replace all files with the new ones.
 
 sudo chown -R "${USERNAME}:${USERNAME}" "${MNT_DIR}/"
 
