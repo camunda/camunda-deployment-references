b67ef17ae9060d936710eb06938516cc42cb30e8
Leo <153937047+leiicamundi@users.noreply.github.com>
2025-08-20 15:48:21 +0200

    wip

--- aws/modules/eks-cluster/cluster.tf
+++ aws/modules/eks-cluster/cluster.tf
@@ -139,37 +139,3 @@ resource "time_sleep" "eks_cluster_warmup" {
 
   depends_on = [module.eks]
 }
-
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    # This requires the awscli to be installed locally where Terraform is executed
-    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-  }
-}
-
-# gp3 storage class
-resource "kubernetes_storage_class_v1" "ebs_sc" {
-  count = var.create_ebs_gp3_default_storage_class ? 1 : 0
-
-  metadata {
-    name = "ebs-sc"
-    annotations = {
-      "storageclass.kubernetes.io/is-default-class" = "true"
-    }
-  }
-  storage_provisioner = "ebs.csi.aws.com"
-  reclaim_policy      = "Retain"
-  parameters = {
-    type = "gp3" # starting eks 1.30, gp3 is the default
-  }
-  volume_binding_mode = "WaitForFirstConsumer"
-
-  depends_on = [
-    time_sleep.eks_cluster_warmup
-  ]
-}
---
+++ aws/modules/eks-storage-class/README.md
@@ -0,0 +1,20 @@
+# eks-storage-class
+
+<!-- BEGIN_TF_DOCS -->
+## Modules
+
+No modules.
+## Resources
+
+| Name | Type |
+|------|------|
+| [kubernetes_storage_class_v1.ebs_sc](https://registry.terraform.io/providers/hashicorp/kubernetes/latest/docs/resources/storage_class_v1) | resource |
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|------|---------|:--------:|
+| <a name="input_cluster_name"></a> [cluster\_name](#input\_cluster\_name) | EKS cluster name | `string` | n/a | yes |
+## Outputs
+
+No outputs.
+<!-- END_TF_DOCS -->
