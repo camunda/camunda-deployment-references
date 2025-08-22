5b92ee9308b2ef8e31afb1120e73b11e92c511ce
Leo <153937047+leiicamundi@users.noreply.github.com>
2025-08-22 17:00:34 +0200

    fix path

--- aws/kubernetes/eks-single-region/procedure/storageclass-configure.sh
+++ aws/kubernetes/eks-single-region/procedure/storageclass-configure.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 kubectl patch storageclass gp2 \
   -p '{"metadata":{"annotations":{"storageclass.kubernetes.io/is-default-class":"false"}}}'
-kubectl apply -f ./manifests/storage-class.yml
+kubectl apply -f ./procedure/manifests/storage-class.yml
