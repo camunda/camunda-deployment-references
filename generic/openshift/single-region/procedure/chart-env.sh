0ef7f32e4ba59866e8a19ebaa3ba431c5fae3107
Langleu <lars.lange@camunda.com>
2025-08-25 11:30:04 +0200

    chore: bump helm chart

--- generic/openshift/single-region/procedure/chart-env.sh
+++ generic/openshift/single-region/procedure/chart-env.sh
@@ -2,7 +2,7 @@
 
 # The Camunda 8 Helm Chart version
 # renovate: datasource=helm depName=camunda-platform versioning=regex:^12(\.(?<minor>\d+))?(\.(?<patch>\d+))?$ registryUrl=https://helm.camunda.io
-export CAMUNDA_HELM_CHART_VERSION="12.0.2"
+export CAMUNDA_HELM_CHART_VERSION="12.4.0"
 
 export CAMUNDA_NAMESPACE="camunda"
 export CAMUNDA_RELEASE_NAME="camunda"
