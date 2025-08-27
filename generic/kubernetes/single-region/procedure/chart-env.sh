2315a6b08ae0157c7dc82b1887ad3399281888d8
Langleu <lars.lange@camunda.com>
2025-08-27 13:06:29 +0200

    fix: renovate annotation ordering to properly update helm chart

--- generic/kubernetes/single-region/procedure/chart-env.sh
+++ generic/kubernetes/single-region/procedure/chart-env.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # The Camunda 8 Helm Chart version
-# renovate: datasource=helm depName=camunda-platform versioning=regex:^12(\.(?<minor>\d+))?(\.(?<patch>\d+))?$ registryUrl=https://helm.camunda.io
-export CAMUNDA_HELM_CHART_VERSION="12.0.2"
+# renovate: datasource=helm depName=camunda-platform registryUrl=https://helm.camunda.io versioning=regex:^12(\.(?<minor>\d+))?(\.(?<patch>\d+))?$
+export CAMUNDA_HELM_CHART_VERSION="12.4.0"
 
 export CAMUNDA_NAMESPACE="camunda"
 export CAMUNDA_RELEASE_NAME="camunda"
