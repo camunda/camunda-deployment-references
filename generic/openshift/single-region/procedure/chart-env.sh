a3b1660299abd2ef05c3e7495baabec4645cb584
Lars Lange <9141483+Langleu@users.noreply.github.com>
2025-08-29 08:25:08 +0200

    fix: renovate annotation ordering to properly update helm chart (#920)

--- generic/openshift/single-region/procedure/chart-env.sh
+++ generic/openshift/single-region/procedure/chart-env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # The Camunda 8 Helm Chart version
-# renovate: datasource=helm depName=camunda-platform versioning=regex:^11(\.(?<minor>\d+))?(\.(?<patch>\d+))?$ registryUrl=https://helm.camunda.io
-export CAMUNDA_HELM_CHART_VERSION="11.3.2"
+# renovate: datasource=helm depName=camunda-platform registryUrl=https://helm.camunda.io versioning=regex:^11(\.(?<minor>\d+))?(\.(?<patch>\d+))?$
+export CAMUNDA_HELM_CHART_VERSION="11.8.0"
