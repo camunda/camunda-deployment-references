7e80f68a00e225f598d1d867d6482d2410269b03
Langleu <lars.lange@camunda.com>
2025-09-08 12:44:20 +0200

    fix: faulty url path for ingress usage

--- generic/kubernetes/single-region/procedure/export-verify-zeebe-domain.sh
+++ generic/kubernetes/single-region/procedure/export-verify-zeebe-domain.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export ZEEBE_ADDRESS_REST="https://$DOMAIN_NAME/core"
+export ZEEBE_ADDRESS_REST="https://$DOMAIN_NAME"
 export ZEEBE_AUTHORIZATION_SERVER_URL="https://$DOMAIN_NAME/auth/realms/camunda-platform/protocol/openid-connect/token"
