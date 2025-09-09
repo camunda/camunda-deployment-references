549f68bdbab83170a762e051078b1dea78fabb3e
Langleu <lars.lange@camunda.com>
2025-09-09 13:59:18 +0200

    chore: rosa single region adjustments

--- generic/openshift/single-region/procedure/chart-env.sh
+++ generic/openshift/single-region/procedure/chart-env.sh
@@ -2,7 +2,7 @@
 
 # The Camunda 8 Helm Chart version
 # renovate: datasource=helm depName=camunda-platform versioning=regex:^13(\.(?<minor>\d+))?(\.(?<patch>\d+))?$ registryUrl=https://helm.camunda.io
-export CAMUNDA_HELM_CHART_VERSION="0.0.0-snapshot-latest"
+export CAMUNDA_HELM_CHART_VERSION="0.0.0-snapshot-alpha"
 # TODO: [release-duty] before the release, update this!
 # TODO: [release-duty] adjust renovate comment to bump the major version
 
--- generic/openshift/single-region/procedure/create-identity-secret.sh
+++ generic/openshift/single-region/procedure/create-identity-secret.sh
@@ -4,9 +4,8 @@ oc create secret generic identity-secret-for-components \
   --namespace "$CAMUNDA_NAMESPACE" \
   --from-literal=connectors-secret="$CONNECTORS_SECRET" \
   --from-literal=console-secret="$CONSOLE_SECRET" \
-  --from-literal=operate-secret="$OPERATE_SECRET" \
+  --from-literal=orchestration-secret="$ORCHESTRATION_SECRET" \
   --from-literal=optimize-secret="$OPTIMIZE_SECRET" \
-  --from-literal=tasklist-secret="$TASKLIST_SECRET" \
-  --from-literal=zeebe-secret="$ZEEBE_SECRET" \
   --from-literal=admin-password="$ADMIN_PASSWORD" \
+  --from-literal=first-user-password="$FIRST_USER_PASSWORD" \
   --from-literal=smtp-password=""
