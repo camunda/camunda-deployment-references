ce6e031666e0047d4127927eec70594b7ffad6e1
Leo <153937047+leiicamundi@users.noreply.github.com>
2025-09-25 20:31:45 +0200

    fix

--- generic/kubernetes/single-region/procedure/create-identity-secret.sh
+++ generic/kubernetes/single-region/procedure/create-identity-secret.sh
@@ -3,9 +3,9 @@
 kubectl create secret generic identity-secret-for-components \
   --namespace "$CAMUNDA_NAMESPACE" \
   --from-literal=identity-connectors-client-token="$CONNECTORS_SECRET" \
-  --from-literal=identit-console-client-token="$CONSOLE_SECRET" \
+  --from-literal=identity-console-client-token="$CONSOLE_SECRET" \
   --from-literal=identity-orchestration-client-token="$ORCHESTRATION_SECRET" \
   --from-literal=identity-optimize-client-token="$OPTIMIZE_SECRET" \
-  --from-literal=identity-admin-client-token="$ADMIN_PASSWORD" \
+  --from-literal=identity-admin-password="$ADMIN_PASSWORD" \
   --from-literal=identity-first-user-password="$FIRST_USER_PASSWORD" \
   --from-literal=smtp-password=""
