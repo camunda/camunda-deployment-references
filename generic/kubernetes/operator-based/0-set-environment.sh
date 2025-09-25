03149831fe804cc8e46010b98f217103cb35a835
Leo <153937047+leiicamundi@users.noreply.github.com>
2025-09-25 15:47:38 +0200

    fix

--- generic/kubernetes/operator-based/0-set-environment.sh
+++ generic/kubernetes/operator-based/0-set-environment.sh
@@ -10,6 +10,6 @@ export CAMUNDA_DOMAIN="${CAMUNDA_DOMAIN:-localhost}"
 
 # The Camunda 8 Helm Chart version
 # renovate: datasource=helm depName=camunda-platform versioning=regex:^12(\.(?<minor>\d+))?(\.(?<patch>\d+))?$ registryUrl=https://helm.camunda.io
-export CAMUNDA_HELM_CHART_VERSION="0.0.0-snapshot-latest"
+export CAMUNDA_HELM_CHART_VERSION="0.0.0-snapshot-alpha"
 # TODO: [release-duty] before the release, update this!
 # TODO: [release-duty] adjust renovate comment to bump the major version
