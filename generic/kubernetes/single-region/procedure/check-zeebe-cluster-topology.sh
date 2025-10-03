cb3b8573e187fe77e6d70013443e6f013a2b1641
Leo <153937047+leiicamundi@users.noreply.github.com>
2025-10-03 19:19:19 +0200

    fix eks

--- generic/kubernetes/single-region/procedure/check-zeebe-cluster-topology.sh
+++ generic/kubernetes/single-region/procedure/check-zeebe-cluster-topology.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
-# Orchestration Cluster basic auth
-TOKEN="Basic ZGVtbzpkZW1v"
-
-if [ -n "${ZEEBE_CLIENT_ID}" ] && [ -n "${ZEEBE_CLIENT_SECRET}" ]; then
-  # Generate a temporary token from the authorization server (keycloak)
-  TOKEN=$(curl --location --request POST "${ZEEBE_AUTHORIZATION_SERVER_URL}" \
-  --header "Content-Type: application/x-www-form-urlencoded" \
-  --data-urlencode "client_id=${ZEEBE_CLIENT_ID}" \
-  --data-urlencode "client_secret=${ZEEBE_CLIENT_SECRET}" \
-  --data-urlencode "grant_type=client_credentials" | jq '.access_token' -r)
-
-  TOKEN="Bearer ${TOKEN}"
-fi
+# Generate a temporary token from the authorization server (keycloak)
+TOKEN=$(curl --location --request POST "${ZEEBE_AUTHORIZATION_SERVER_URL}" \
+--header "Content-Type: application/x-www-form-urlencoded" \
+--data-urlencode "client_id=${ZEEBE_CLIENT_ID}" \
+--data-urlencode "client_secret=${ZEEBE_CLIENT_SECRET}" \
+--data-urlencode "grant_type=client_credentials" | jq '.access_token' -r)
 
 # Show the zeebe cluster topology
 echo "📡 Fetching Zeebe cluster topology..."
