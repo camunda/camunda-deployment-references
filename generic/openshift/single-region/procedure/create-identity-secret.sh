71fe6ffcbf12824e152b3f637d34caa1aa418f13
Leo <153937047+leiicamundi@users.noreply.github.com>
2025-10-03 18:59:45 +0200

    update openshift single region values

--- generic/openshift/single-region/procedure/create-identity-secret.sh
+++ generic/openshift/single-region/procedure/create-identity-secret.sh
@@ -4,8 +4,11 @@ oc create secret generic identity-secret-for-components \
   --namespace "$CAMUNDA_NAMESPACE" \
   --from-literal=identity-connectors-client-token="$CONNECTORS_SECRET" \
   --from-literal=identity-console-client-token="$CONSOLE_SECRET" \
+  --from-literal=identity-webmodeler-client-token="$WEB_MODELER_SECRET" \
   --from-literal=identity-orchestration-client-token="$ORCHESTRATION_SECRET" \
   --from-literal=identity-optimize-client-token="$OPTIMIZE_SECRET" \
   --from-literal=identity-admin-client-token="$ADMIN_PASSWORD" \
   --from-literal=identity-first-user-password="$FIRST_USER_PASSWORD" \
+  --from-literal=identity-keycloak-pg-admin-password-key="$KEYCLOAK_PSQL_ADMIN_PASSWORD"  \
+  --from-literal=identity-keycloak-pg-user-password-key="$KEYCLOAK_PSQL_USER_PASSWORD"  \
   --from-literal=smtp-password=""
