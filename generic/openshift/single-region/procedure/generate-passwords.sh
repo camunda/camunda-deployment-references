b1f774c9dad0a4fe01cc99c33c51b6bc3877016d
Leo <153937047+leiicamundi@users.noreply.github.com>
2025-10-06 21:03:13 +0200

    fix pg passwords

--- generic/openshift/single-region/procedure/generate-passwords.sh
+++ generic/openshift/single-region/procedure/generate-passwords.sh
@@ -2,10 +2,12 @@
 
 export CONNECTORS_SECRET="$(openssl rand -hex 16)"
 export CONSOLE_SECRET="$(openssl rand -hex 16)"
-export WEB_MODELER_SECRET="$(openssl rand -hex 16)"
 export ORCHESTRATION_SECRET="$(openssl rand -hex 16)"
 export OPTIMIZE_SECRET="$(openssl rand -hex 16)"
 export ADMIN_PASSWORD="$(openssl rand -hex 16)"
 export KEYCLOAK_PSQL_ADMIN_PASSWORD="$(openssl rand -hex 16)"
 export KEYCLOAK_PSQL_USER_PASSWORD="$(openssl rand -hex 16)"
 export FIRST_USER_PASSWORD="$(openssl rand -hex 16)"
+export WEB_MODELER_SECRET="$(openssl rand -hex 16)"
+export WEB_MODELER_PSQL_ADMIN_PASSWORD="$(openssl rand -hex 16)"
+export WEB_MODELER_PSQL_USER_PASSWORD="$(openssl rand -hex 16)"
