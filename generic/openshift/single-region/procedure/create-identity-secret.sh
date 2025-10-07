770bfab8b03f90917c9b26ceab7461291a80256b
Leo <153937047+leiicamundi@users.noreply.github.com>
2025-10-07 13:00:57 +0200

    adjust identity secret

--- generic/openshift/single-region/procedure/create-identity-secret.sh
+++ generic/openshift/single-region/procedure/create-identity-secret.sh
@@ -6,7 +6,6 @@ oc create secret generic identity-secret-for-components \
   --namespace "$CAMUNDA_NAMESPACE" \
   --from-literal=identity-connectors-client-token="$CONNECTORS_SECRET" \
   --from-literal=identity-console-client-token="$CONSOLE_SECRET" \
-  --from-literal=identity-identity-client-token="$IDENTITY_SECRET" \
   --from-literal=identity-orchestration-client-token="$ORCHESTRATION_SECRET" \
   --from-literal=identity-optimize-client-token="$OPTIMIZE_SECRET" \
   --from-literal=identity-admin-client-token="$ADMIN_PASSWORD" \
