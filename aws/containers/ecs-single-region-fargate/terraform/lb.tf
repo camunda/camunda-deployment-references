1f8fdf02100d1640acf7d7838f3a4bdab8dd628e
Carlo Sana <carlo.sana@camunda.com>
2025-10-29 14:08:03 +0100

    feat: remove prometheus from LB

--- aws/containers/ecs-single-region-fargate/terraform/lb.tf
+++ aws/containers/ecs-single-region-fargate/terraform/lb.tf
@@ -82,27 +82,27 @@ resource "aws_lb_target_group" "grafana_3000" {
 
 
 # Prometheus target group
-resource "aws_lb_target_group" "prometheus_9090" {
-  count = var.enable_alb ? 1 : 0
-
-  name        = "${var.prefix}-tg-prom-9090-${count.index}"
-  port        = 9090
-  protocol    = "HTTP"
-  vpc_id      = module.vpc.vpc_id
-  target_type = "ip"
-
-  deregistration_delay = 30
-
-  health_check {
-    path                = "/-/ready"
-    port                = "9090"
-    protocol            = "HTTP"
-    timeout             = 5
-    interval            = 30
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-  }
-}
+# resource "aws_lb_target_group" "prometheus_9090" {
+#   count = var.enable_alb ? 1 : 0
+#
+#   name        = "${var.prefix}-tg-prom-9090-${count.index}"
+#   port        = 9090
+#   protocol    = "HTTP"
+#   vpc_id      = module.vpc.vpc_id
+#   target_type = "ip"
+#
+#   deregistration_delay = 30
+#
+#   health_check {
+#     path                = "/-/ready"
+#     port                = "9090"
+#     protocol            = "HTTP"
+#     timeout             = 5
+#     interval            = 30
+#     healthy_threshold   = 2
+#     unhealthy_threshold = 2
+#   }
+# }
 
 # Prometheus target group
 resource "aws_lb_target_group" "nginx_4000" {
@@ -161,7 +161,7 @@ resource "aws_lb" "main" {
     aws_security_group.allow_remote_80_443.id,
     # aws_security_group.allow_remote_9090.id,
     aws_security_group.allow_necessary_camunda_ports_within_vpc.id,
-    # aws_security_group.allow_remote_3000.id,
+    aws_security_group.allow_remote_3000.id,
     aws_security_group.allow_remote_9600.id,
     aws_security_group.allow_remote_4000.id
   ]
@@ -209,18 +209,18 @@ resource "aws_lb_listener" "http_8080" {
 }
 
 # Prometheus listener (optional exposure)
-resource "aws_lb_listener" "http_9090" {
-  count = var.enable_alb ? 1 : 0
-
-  load_balancer_arn = aws_lb.main[count.index].arn
-  port              = "9090"
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.prometheus_9090[count.index].arn
-  }
-}
+# resource "aws_lb_listener" "http_9090" {
+#   count = var.enable_alb ? 1 : 0
+#
+#   load_balancer_arn = aws_lb.main[count.index].arn
+#   port              = "9090"
+#   protocol          = "HTTP"
+#
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.prometheus_9090[count.index].arn
+#   }
+# }
 
 # core management api
 resource "aws_lb_listener" "http_9600" {
