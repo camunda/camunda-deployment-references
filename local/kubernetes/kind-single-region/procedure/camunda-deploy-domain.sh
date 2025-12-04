42254858e41e62e9984eae79ee18ca6e75b330b1
Leo <153937047+leiicamundi@users.noreply.github.com>
2025-12-04 22:37:38 +0100

    fix

--- local/kubernetes/kind-single-region/procedure/camunda-deploy-domain.sh
+++ local/kubernetes/kind-single-region/procedure/camunda-deploy-domain.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 set -euo pipefail
 
+# renovate: datasource=helm depName=camunda-platform versioning=regex:^12(\.(?<minor>\d+))?(\.(?<patch>\d+))?$ registryUrl=https://helm.camunda.io
+export CAMUNDA_HELM_CHART_VERSION="0.0.0-snapshot-alpha"
+# TODO: [release-duty] before the release, update this!
+# TODO: [release-duty] adjust renovate comment to bump the major version
+
 # Deploy Camunda Platform (domain mode with TLS)
 # Run from: local/kubernetes/kind-single-region/
 
 echo "Installing Camunda Platform (domain mode)..."
 
-helm repo add camunda https://helm.camunda.io
-helm repo update camunda
-
-helm upgrade --install camunda camunda/camunda-platform \
-    --namespace camunda \
+helm upgrade --install "camunda" oci://ghcr.io/camunda/helm/camunda-platform \
+    --version "$CAMUNDA_HELM_CHART_VERSION" \
+    --namespace "camunda" \
     --values helm-values/values-domain.yml \
     --values helm-values/values-mkcert.yml
+
+# TODO: [release-duty] before the release, update this by removing the oci pull above
+# and uncomment the installation instruction below
+
+# helm upgrade --install "camunda" camunda-platform \
+#     --repo https://helm.camunda.io \
+#     --version "$CAMUNDA_HELM_CHART_VERSION" \
+#     --namespace "camunda" \
+#     --values helm-values/values-domain.yml \
+#     --values helm-values/values-mkcert.yml
 
 echo ""
 echo "Camunda Platform deployed!"
--- local/kubernetes/kind-single-region/procedure/camunda-deploy-no-domain.sh
+++ local/kubernetes/kind-single-region/procedure/camunda-deploy-no-domain.sh
@@ -4,14 +4,26 @@ set -euo pipefail
 # Deploy Camunda Platform (no-domain mode with port-forward)
 # Run from: local/kubernetes/kind-single-region/
 
+# renovate: datasource=helm depName=camunda-platform versioning=regex:^12(\.(?<minor>\d+))?(\.(?<patch>\d+))?$ registryUrl=https://helm.camunda.io
+export CAMUNDA_HELM_CHART_VERSION="0.0.0-snapshot-alpha"
+# TODO: [release-duty] before the release, update this!
+# TODO: [release-duty] adjust renovate comment to bump the major version
+
 echo "Installing Camunda Platform (no-domain mode)..."
 
-helm repo add camunda https://helm.camunda.io
-helm repo update camunda
-
-helm upgrade --install camunda camunda/camunda-platform \
-    --namespace camunda \
+helm upgrade --install "camunda" oci://ghcr.io/camunda/helm/camunda-platform \
+    --version "$CAMUNDA_HELM_CHART_VERSION" \
+    --namespace "camunda" \
     --values helm-values/values-no-domain.yml
+
+# TODO: [release-duty] before the release, update this by removing the oci pull above
+# and uncomment the installation instruction below
+
+# helm upgrade --install "camunda" camunda-platform \
+#     --repo https://helm.camunda.io \
+#     --version "$CAMUNDA_HELM_CHART_VERSION" \
+#     --namespace "camunda" \
+#     --values helm-values/values-no-domain.yml
 
 echo ""
 echo "Camunda Platform deployed!"
