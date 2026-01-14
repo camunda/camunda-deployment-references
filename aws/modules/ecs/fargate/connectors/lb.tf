8fa563e5dcd64ce24b4e7683412be890374a1b17
Langleu <lars.lange@camunda.com>
2026-01-14 11:05:30 +0100

    fix: target group sizing and clean up some unnecessary steps

--- aws/modules/ecs/fargate/connectors/lb.tf
+++ aws/modules/ecs/fargate/connectors/lb.tf
@@ -1,5 +1,5 @@
 resource "aws_lb_target_group" "main" {
-  name        = "${var.prefix}-connectors-tg-8080"
+  name        = "${var.prefix}-con-tg-8080"
   port        = 8080
   protocol    = "HTTP"
   vpc_id      = var.vpc_id
