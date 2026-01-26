8f69b9e7b7e08083f1d195cbfa0459d5d21565be
Langleu <lars.lange@camunda.com>
2026-01-26 10:53:31 +0100

    chore: implement requested changes

--- aws/modules/ecs/fargate/connectors/lb.tf
+++ aws/modules/ecs/fargate/connectors/lb.tf
@@ -1,5 +1,5 @@
 resource "aws_lb_target_group" "main" {
-  name        = "${var.prefix}-con-tg-8080"
+  name        = "${substr(var.prefix, 0, 17)}-con-tg-8080"
   port        = 8080
   protocol    = "HTTP"
   vpc_id      = var.vpc_id
