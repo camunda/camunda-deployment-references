4cd536513f3839de1fcfa92463e37e1b8eef47e6
Langleu <lars.lange@camunda.com>
2026-02-06 10:58:18 +0100

    chore: add ecs ip warning to VPC creation
    same as we have it present in EKS

--- aws/containers/ecs-single-region-fargate/terraform/cluster/vpc.tf
+++ aws/containers/ecs-single-region-fargate/terraform/cluster/vpc.tf
@@ -7,6 +7,30 @@ locals {
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
 }
 
+data "aws_servicequotas_service_quota" "elastic_ip_quota" {
+  service_code = "ec2"
+  quota_code   = "L-0263D0A3" # Quota code for Elastic IP addresses per region
+}
+
+data "aws_eips" "current_usage" {}
+
+# Data source to check if the VPC exists
+data "aws_vpcs" "current_vpcs" {
+  tags = {
+    Name = local.name
+  }
+}
+
+check "elastic_ip_quota_check" {
+  # Only check the condition when no existing vpc is there
+  assert {
+    condition = length(data.aws_vpcs.current_vpcs.ids) > 0 || (
+      (data.aws_servicequotas_service_quota.elastic_ip_quota.value - length(data.aws_eips.current_usage.public_ips)) >= length(local.azs)
+    )
+    error_message = "Not enough available Elastic IPs to cover required NAT gateways (need: ${length(local.azs)}, have: ${(data.aws_servicequotas_service_quota.elastic_ip_quota.value - length(data.aws_eips.current_usage.public_ips))})."
+  }
+}
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "v6.6.0"
