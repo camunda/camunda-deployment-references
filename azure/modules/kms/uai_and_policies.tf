ac184581ade981c8a26b57c0a3f8348f93c9b37d
Leo <153937047+leiicamundi@users.noreply.github.com>
2026-02-06 10:34:25 +0100

    fix(azure): deprecation of vault key vault

--- azure/modules/kms/uai_and_policies.tf
+++ azure/modules/kms/uai_and_policies.tf
@@ -1,45 +1,8 @@
+# User Assigned Identity for AKS to access Key Vault
+# Access is managed via RBAC role assignments (see role_assignments.tf)
+# Reference: https://learn.microsoft.com/azure/key-vault/general/rbac-migration
 resource "azurerm_user_assigned_identity" "this" {
   name                = var.uai_name
   resource_group_name = var.resource_group_name
   location            = var.location
-}
-
-# AKS needs wrap/unwrap
-resource "azurerm_key_vault_access_policy" "aks_kms" {
-  key_vault_id = azurerm_key_vault.this.id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = azurerm_user_assigned_identity.this.principal_id
-
-  key_permissions = [
-    "Get",
-    "List",
-    "Create",
-    "Delete",
-    "Update",
-    "Import",
-    "Encrypt",
-    "Decrypt",
-    "WrapKey",
-    "UnwrapKey",
-  ]
-}
-
-# Terraform SP needs full key rights
-resource "azurerm_key_vault_access_policy" "tf_kv" {
-  key_vault_id = azurerm_key_vault.this.id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = data.azuread_service_principal.terraform_sp.object_id
-
-  key_permissions = [
-    "Get",
-    "List",
-    "Create",
-    "Delete",
-    "WrapKey",
-    "UnwrapKey",
-    "GetRotationPolicy",
-    "SetRotationPolicy",
-    "Update",
-    "Rotate",
-  ]
 }
