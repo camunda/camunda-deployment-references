892d06ba43ab082fb43b7af677d647dd8118bf3e
Leo <153937047+leiicamundi@users.noreply.github.com>
2026-03-13 18:23:37 +0100

    fix: update cluster count

--- aws/openshift/rosa-hcp-dual-region/procedure/gather-cluster-login-id.sh
+++ aws/openshift/rosa-hcp-dual-region/procedure/gather-cluster-login-id.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Cluster 0
+export CLUSTER_0_NAME="$(terraform console <<<local.rosa_cluster_0_name | tail -n 1 | jq -r)"
+export CLUSTER_0_API_URL=$(terraform output -raw cluster_0_openshift_api_url)
+export CLUSTER_0_ADMIN_USERNAME="$(terraform console <<<local.rosa_cluster_0_admin_username | tail -n 1 | jq -r)"
+export CLUSTER_0_ADMIN_PASSWORD="$(terraform console <<<local.rosa_cluster_0_admin_password | tail -n 1 | jq -r)"
+
+echo "CLUSTER_0_NAME=$CLUSTER_0_NAME"
+echo "CLUSTER_0_API_URL=$CLUSTER_0_API_URL"
+echo "CLUSTER_0_ADMIN_USERNAME=$CLUSTER_0_ADMIN_USERNAME"
+
 # Cluster 1
 export CLUSTER_1_NAME="$(terraform console <<<local.rosa_cluster_1_name | tail -n 1 | jq -r)"
 export CLUSTER_1_API_URL=$(terraform output -raw cluster_1_openshift_api_url)
@@ -9,13 +19,3 @@ export CLUSTER_1_ADMIN_PASSWORD="$(terraform console <<<local.rosa_cluster_1_adm
 echo "CLUSTER_1_NAME=$CLUSTER_1_NAME"
 echo "CLUSTER_1_API_URL=$CLUSTER_1_API_URL"
 echo "CLUSTER_1_ADMIN_USERNAME=$CLUSTER_1_ADMIN_USERNAME"
-
-# Cluster 2
-export CLUSTER_2_NAME="$(terraform console <<<local.rosa_cluster_2_name | tail -n 1 | jq -r)"
-export CLUSTER_2_API_URL=$(terraform output -raw cluster_2_openshift_api_url)
-export CLUSTER_2_ADMIN_USERNAME="$(terraform console <<<local.rosa_cluster_2_admin_username | tail -n 1 | jq -r)"
-export CLUSTER_2_ADMIN_PASSWORD="$(terraform console <<<local.rosa_cluster_2_admin_password | tail -n 1 | jq -r)"
-
-echo "CLUSTER_2_NAME=$CLUSTER_2_NAME"
-echo "CLUSTER_2_API_URL=$CLUSTER_2_API_URL"
-echo "CLUSTER_2_ADMIN_USERNAME=$CLUSTER_2_ADMIN_USERNAME"
