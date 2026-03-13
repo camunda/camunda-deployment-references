c539baf2ec47bb65640025b3c554d40d48a5fc0e
Leo <153937047+leiicamundi@users.noreply.github.com>
2026-03-13 15:01:16 +0100

    fix indices

--- generic/openshift/dual-region/procedure/assemble-envsubst-values.sh
+++ generic/openshift/dual-region/procedure/assemble-envsubst-values.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ZEEBE_SERVICE_NAME="$REGION_0_ZEEBE_SERVICE_NAME" DOLLAR="\$" envsubst < values-region-0.yml > generated-values-region-0.yml
+ZEEBE_SERVICE_NAME="$CLUSTER_1_REGION_ZEEBE_SERVICE_NAME" DOLLAR="\$" envsubst < values-region-0.yml > generated-values-region-0.yml
 cat generated-values-region-0.yml
 
-ZEEBE_SERVICE_NAME="$REGION_1_ZEEBE_SERVICE_NAME" DOLLAR="\$" envsubst < values-region-1.yml > generated-values-region-1.yml
+ZEEBE_SERVICE_NAME="$CLUSTER_2_REGION_ZEEBE_SERVICE_NAME" DOLLAR="\$" envsubst < values-region-1.yml > generated-values-region-1.yml
 cat generated-values-region-1.yml
