4284dfada499cef7310cfbe7025fb1e2fef3c958
Leo <153937047+leiicamundi@users.noreply.github.com>
2026-04-02 19:23:02 +0200

    fix: quote terraform output substitutions in vars-create-db.sh

--- aws/kubernetes/eks-single-region/procedure/vars-create-db.sh
+++ aws/kubernetes/eks-single-region/procedure/vars-create-db.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-export AURORA_ENDPOINT=$(terraform output -raw postgres_endpoint)
+export AURORA_ENDPOINT="$(terraform output -raw postgres_endpoint)"
 export AURORA_PORT=5432
 
 # PostgreSQL Credentials
-export AURORA_USERNAME=$(terraform output -raw aurora_master_username)
-export AURORA_PASSWORD=$(terraform output -raw aurora_master_password)
+export AURORA_USERNAME="$(terraform output -raw aurora_master_username)"
+export AURORA_PASSWORD="$(terraform output -raw aurora_master_password)"
 
 # PostgreSQL Major Version (e.g. 17)
-export POSTGRES_MAJOR_VERSION=$(terraform output -raw postgres_major_version)
+export POSTGRES_MAJOR_VERSION="$(terraform output -raw postgres_major_version)"
