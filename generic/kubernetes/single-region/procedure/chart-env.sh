8f52d7c86f17c8f0a84a870f63b00bb9a66f0dd1
Leo <153937047+leiicamundi@users.noreply.github.com>
2026-04-17 15:42:20 +0200

    fix: correct renovate chart regex from ^15 to ^14 on stable/8.9 (#2319)

--- generic/kubernetes/single-region/procedure/chart-env.sh
+++ generic/kubernetes/single-region/procedure/chart-env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # The Camunda 8 Helm Chart version
-# renovate: datasource=helm depName=camunda-platform versioning=regex:^15(\.(?<minor>\d+))?(\.(?<patch>\d+))?$ registryUrl=https://helm.camunda.io
+# renovate: datasource=helm depName=camunda-platform versioning=regex:^14(\.(?<minor>\d+))?(\.(?<patch>\d+))?$ registryUrl=https://helm.camunda.io
 export CAMUNDA_HELM_CHART_VERSION="14.0.0"
 
 export CAMUNDA_NAMESPACE="camunda"
