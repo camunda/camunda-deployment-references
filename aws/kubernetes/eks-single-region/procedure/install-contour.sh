b3ea5af0e02d3858b4404235c3762bfb4434185a
Marcel Dias <marcel.dias@camunda.com>
2026-06-08 19:58:07 +0200

    fix(eks-sr): do not mark Contour IngressClass as cluster default
    Ingresses set className: contour explicitly, and the previous
    ingress-nginx setup never claimed the default IngressClass, so keep
    parity and avoid Contour silently capturing class-less Ingresses.

--- aws/kubernetes/eks-single-region/procedure/install-contour.sh
+++ aws/kubernetes/eks-single-region/procedure/install-contour.sh
@@ -5,6 +5,7 @@ helm upgrade --install \
   contour contour \
   --repo https://projectcontour.github.io/helm-charts/ \
   --version "$CONTOUR_HELM_CHART_VERSION" \
+  --set 'contour.ingressClass.default=false' \
   --set-string 'envoy.service.annotations.service\.beta\.kubernetes\.io\/aws-load-balancer-backend-protocol=tcp' \
   --set-string 'envoy.service.annotations.service\.beta\.kubernetes\.io\/aws-load-balancer-cross-zone-load-balancing-enabled=true' \
   --set-string 'envoy.service.annotations.service\.beta\.kubernetes\.io\/aws-load-balancer-type=nlb' \
