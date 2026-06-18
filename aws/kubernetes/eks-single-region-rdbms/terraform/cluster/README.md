3eb4e65c7a6141f20cb0d89478bf0e9a8eb5e538
Leo <153937047+leiicamundi@users.noreply.github.com>
2026-06-18 10:48:58 +0200

    feat(eks): add RDBMS secondary storage variant for EKS single-region
    Add the `eks-single-region-rdbms` reference architecture: an EKS single-region
    variant that uses Amazon Aurora PostgreSQL as the orchestration secondary storage
    instead of Amazon OpenSearch.
    
    - terraform/cluster: symlink the shared EKS cluster + config, override db.tf to add
      the `camunda_orchestration` database/user/password, and omit opensearch.tf
      (no OpenSearch domain is provisioned in RDBMS mode)
    - helm-values: RDBMS secondary storage (`orchestration.data.secondaryStorage.type: rdbms`),
      Optimize and Elasticsearch disabled, OpenSearch references removed
    - procedure: override export-helm-values.sh, check-env-variables.sh,
      create-external-db-secrets.sh, create-setup-db-secret.sh and setup-postgres-create-db.yml
      to provision and wire the orchestration database (orchestration-postgres-secret);
      symlink the remaining shared scripts
    - get-your-copy.sh points at the new directory
    
    Golden plan and the integration-test scenario wiring are follow-ups (require AWS CI).
    
    Part of camunda/team-infrastructure-experience#1063
    Part of camunda/team-infrastructure-experience#1145
    Closes #2079

---
+++ aws/kubernetes/eks-single-region-rdbms/terraform/cluster/README.md
@@ -0,0 +1,59 @@
+# Camunda 8 on AWS EKS
+
+This folder describes the IaC of Camunda 8 on AWS EKS.
+Instructions can be found on the official documentation: https://docs.camunda.io/docs/self-managed/setup/deploy/amazon/amazon-eks/eks-terraform/
+
+- [AWS Elastic Kubernetes Service](https://aws.amazon.com/eks/)
+
+<!-- BEGIN_TF_DOCS -->
+## Requirements
+
+| Name | Version |
+| ---- | ------- |
+| <a name="requirement_terraform"></a> [terraform](#requirement\_terraform) | >= 1.0 |
+| <a name="requirement_aws"></a> [aws](#requirement\_aws) | ~> 6.0 |
+| <a name="requirement_random"></a> [random](#requirement\_random) | ~> 3.0 |
+
+## Providers
+
+| Name | Version |
+| ---- | ------- |
+| <a name="provider_random"></a> [random](#provider\_random) | 3.9.0 |
+
+## Modules
+
+| Name | Source | Version |
+| ---- | ------ | ------- |
+| <a name="module_eks_cluster"></a> [eks\_cluster](#module\_eks\_cluster) | ../../../../modules/eks-cluster | n/a |
+| <a name="module_postgresql"></a> [postgresql](#module\_postgresql) | ../../../../modules/aurora | n/a |
+
+## Resources
+
+| Name | Type |
+| ---- | ---- |
+| [random_password.aurora_admin](https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/password) | resource |
+| [random_password.identity_db](https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/password) | resource |
+| [random_password.orchestration_db](https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/password) | resource |
+| [random_password.webmodeler_db](https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/password) | resource |
+
+## Inputs
+
+| Name | Description | Type | Default | Required |
+| ---- | ----------- | ---- | ------- | :------: |
+| <a name="input_default_tags"></a> [default\_tags](#input\_default\_tags) | Default tags to apply to all resources | `map(string)` | `{}` | no |
+
+## Outputs
+
+| Name | Description |
+| ---- | ----------- |
+| <a name="output_aurora_master_password"></a> [aurora\_master\_password](#output\_aurora\_master\_password) | Aurora admin password |
+| <a name="output_aurora_master_username"></a> [aurora\_master\_username](#output\_aurora\_master\_username) | Aurora admin username |
+| <a name="output_camunda_identity_db_password"></a> [camunda\_identity\_db\_password](#output\_camunda\_identity\_db\_password) | Identity DB password |
+| <a name="output_camunda_orchestration_db_password"></a> [camunda\_orchestration\_db\_password](#output\_camunda\_orchestration\_db\_password) | Orchestration (RDBMS secondary storage) DB password |
+| <a name="output_camunda_webmodeler_db_password"></a> [camunda\_webmodeler\_db\_password](#output\_camunda\_webmodeler\_db\_password) | WebModeler DB password |
+| <a name="output_cert_manager_arn"></a> [cert\_manager\_arn](#output\_cert\_manager\_arn) | The Amazon Resource Name (ARN) of the AWS IAM Roles for Service Account mapping for the cert-manager |
+| <a name="output_external_dns_arn"></a> [external\_dns\_arn](#output\_external\_dns\_arn) | The Amazon Resource Name (ARN) of the AWS IAM Roles for Service Account mapping for the external-dns |
+| <a name="output_postgres_endpoint"></a> [postgres\_endpoint](#output\_postgres\_endpoint) | The Postgres endpoint URL |
+| <a name="output_postgres_major_version"></a> [postgres\_major\_version](#output\_postgres\_major\_version) | PostgreSQL major version (derived from Aurora engine\_version) |
+| <a name="output_vpc_id"></a> [vpc\_id](#output\_vpc\_id) | The ID of the Virtual Private Cloud (VPC) where the cluster and related resources are deployed. |
+<!-- END_TF_DOCS -->
---
+++ aws/kubernetes/eks-single-region-rdbms/terraform/cluster/db.tf
@@ -0,0 +1,133 @@
+locals {
+  aurora_cluster_name = "cluster-name-pg-rdbms" # Replace "cluster-name" with your cluster's name
+
+  aurora_master_username = "c8admin" # Aurora admin username
+  aurora_master_password = random_password.aurora_admin.result
+
+  # Database names for Camunda components
+  camunda_database_identity   = "camunda_identity"   # Name of your camunda database for Identity
+  camunda_database_webmodeler = "camunda_webmodeler" # Name of your camunda database for WebModeler
+
+  # RDBMS secondary storage: dedicated database for the Orchestration cluster
+  # (Operate/Tasklist/Zeebe exporter) used instead of Elasticsearch/OpenSearch.
+  camunda_database_orchestration = "camunda_orchestration" # Name of your camunda database for orchestration secondary storage
+
+  # Connection configuration
+  camunda_identity_db_username   = "identity_db"   # Username for connection to the Identity DB
+  camunda_webmodeler_db_username = "webmodeler_db" # Username for connection to the WebModeler DB
+
+  # RDBMS secondary storage: dedicated user for the orchestration DB
+  camunda_orchestration_db_username = "orchestration_db" # Username for connection to the orchestration DB
+
+  camunda_identity_db_password      = random_password.identity_db.result
+  camunda_webmodeler_db_password    = random_password.webmodeler_db.result
+  camunda_orchestration_db_password = random_password.orchestration_db.result
+
+  db_tags = {} # additional tags that you may want to apply to the resources
+}
+
+# Generate random passwords for database credentials
+# To retrieve passwords after apply: terraform output -json | jq '.aurora_master_password.value, .camunda_identity_db_password.value, .camunda_webmodeler_db_password.value, .camunda_orchestration_db_password.value'
+resource "random_password" "aurora_admin" {
+  length           = 24
+  special          = true
+  override_special = "!#%&*()-_=+[]{}:?"
+  min_upper        = 1
+  min_lower        = 1
+  min_numeric      = 1
+  min_special      = 1
+}
+
+resource "random_password" "identity_db" {
+  length           = 24
+  special          = true
+  override_special = "!#%&*()-_=+[]{}:?"
+  min_upper        = 1
+  min_lower        = 1
+  min_numeric      = 1
+  min_special      = 1
+}
+
+resource "random_password" "webmodeler_db" {
+  length           = 24
+  special          = true
+  override_special = "!#%&*()-_=+[]{}:?"
+  min_upper        = 1
+  min_lower        = 1
+  min_numeric      = 1
+  min_special      = 1
+}
+
+resource "random_password" "orchestration_db" {
+  length           = 24
+  special          = true
+  override_special = "!#%&*()-_=+[]{}:?"
+  min_upper        = 1
+  min_lower        = 1
+  min_numeric      = 1
+  min_special      = 1
+}
+
+module "postgresql" {
+  source = "../../../../modules/aurora"
+  # renovate: datasource=custom.aurora-pg-camunda depName=aurora-postgresql versioning=loose
+  engine_version             = "17.9"
+  auto_minor_version_upgrade = false
+  cluster_name               = local.aurora_cluster_name
+  default_database_name      = local.camunda_database_identity
+
+  # create each AZs
+  availability_zones = ["${local.eks_cluster_region}a", "${local.eks_cluster_region}b", "${local.eks_cluster_region}c"]
+
+  username = local.aurora_master_username
+  password = local.aurora_master_password
+
+  vpc_id      = module.eks_cluster.vpc_id
+  subnet_ids  = module.eks_cluster.private_subnet_ids
+  cidr_blocks = concat(module.eks_cluster.private_vpc_cidr_blocks, module.eks_cluster.public_vpc_cidr_blocks)
+
+  num_instances  = "1" # only one instance, you can add add other read-only instances if you want
+  instance_class = "db.t3.medium"
+
+  tags       = local.db_tags
+  depends_on = [module.eks_cluster]
+}
+
+output "postgres_endpoint" {
+  value       = module.postgresql.aurora_endpoint
+  description = "The Postgres endpoint URL"
+}
+
+output "aurora_master_username" {
+  description = "Aurora admin username"
+  value       = local.aurora_master_username
+}
+
+output "aurora_master_password" {
+  description = "Aurora admin password"
+  value       = local.aurora_master_password
+  sensitive   = true
+}
+
+output "camunda_identity_db_password" {
+  description = "Identity DB password"
+  value       = local.camunda_identity_db_password
+  sensitive   = true
+}
+
+output "camunda_webmodeler_db_password" {
+  description = "WebModeler DB password"
+  value       = local.camunda_webmodeler_db_password
+  sensitive   = true
+}
+
+output "camunda_orchestration_db_password" {
+  description = "Orchestration (RDBMS secondary storage) DB password"
+  value       = local.camunda_orchestration_db_password
+  sensitive   = true
+}
+
+output "postgres_major_version" {
+  description = "PostgreSQL major version (derived from Aurora engine_version)"
+  value       = split(".", module.postgresql.aurora_engine_version)[0]
+}
