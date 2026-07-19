bb630c81d3e1432ea6d5657ecfd8cd207d3214be
Marcel Dias <marcel.dias@camunda.com>
2026-07-19 15:07:44 +0200

    refactor(ecs): derive management identity service-connect output from resource

--- aws/modules/ecs/fargate/management-identity/output.tf
+++ aws/modules/ecs/fargate/management-identity/output.tf
@@ -1,4 +1,4 @@
 output "identity_service_connect" {
   description = "Service Connect DNS name for Management Identity (reachable at this name on port 8084 within the ECS cluster)."
-  value       = "identity"
+  value       = aws_ecs_service.management_identity.service_connect_configuration[0].service[0].discovery_name
 }
